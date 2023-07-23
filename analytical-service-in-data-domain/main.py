900a4d498ff7719efdd8d2fe1815a8d17eb77b99
Pouya <pouya.ataei@ezyvet.com>
2023-07-23 21:20:34 +1200

    finalised the consumer and producer

--- analytical-service-in-data-domain/main.py
+++ analytical-service-in-data-domain/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 import requests
 import json
 
 app = FastAPI()
+
+consumer_base_url = None
 
 
 @app.on_event("startup")
@@ -17,13 +19,58 @@ async def startup_event():
         "auto.offset.reset": "earliest",
         "auto.commit.enable": "false"
     }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-
-    if response.status_code != 200:
-        raise Exception("Failed to create Kafka consumer: " + response.text)
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        # will raise an HTTPError if the status code is 4xx or 5xx
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        if response.status_code == 409:
+            print("Kafka consumer already exists. Proceeding...")
+        else:
+            raise Exception("Failed to create Kafka consumer: " + str(e))
     else:
         print("Kafka consumer created successfully")
+        consumer_info = response.json()
+        print("Consumer instance URI: " + consumer_info['base_uri'])
+        global consumer_base_url
+        consumer_base_url = consumer_info['base_uri'].replace(
+            'http://', 'http://localhost/')
 
-    # The response will contain the base_uri for this consumer instance
-    consumer_info = response.json()
-    print("Consumer instance URI: " + consumer_info['base_uri'])
+        # Subscribe the consumer to the topic
+        url = consumer_base_url + "/subscription"
+        headers = {'Content-Type': 'application/vnd.kafka.v2+json'}
+        data = {"topics": ["test"]}  # Adjusted the topic name here
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        if response.status_code != 204:
+            raise Exception(
+                "Failed to subscribe consumer to topic: " + response.text)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    url = consumer_base_url
+    response = requests.delete(url)
+    print(f"Consumer deleted with status code {response.status_code}")
+
+
+@app.get("/consume")
+async def consume_kafka_message(background_tasks: BackgroundTasks):
+    if consumer_base_url is None:
+        return {"status": "Consumer has not been initialized. Please try again later."}
+
+    print(consumer_base_url)
+    url = consumer_base_url + "/records"
+    headers = {"Accept": "application/vnd.kafka.binary.v2+json"}
+
+    def consume_records():
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            raise Exception(f"GET /records/ did not succeed: {response.text}")
+        else:
+            records = response.json()
+            for record in records:
+                print(
+                    f"Consumed record with key {record['key']} and value {record['value']} from topic {record['topic']}")
+
+    background_tasks.add_task(consume_records)
+    return {"status": "Consuming records in the background"}
