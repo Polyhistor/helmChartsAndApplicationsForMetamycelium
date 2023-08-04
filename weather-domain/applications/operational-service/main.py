59dc03c7476a7e3f26cab8de48acfa8e5fa7b063
Pouya Ataei <pouya-ataei@idexx.com>
2023-08-04 20:51:48 +1200

    figured out to create a topic using kafka rest proxy

--- weather-domain/applications/operational-service/main.py
+++ weather-domain/applications/operational-service/main.py
@@ -11,6 +11,19 @@ df2 = pd.read_csv('precipitation.csv')
 
 # Merge the dataframes on the 'date' column
 merged_df = pd.merge(df1, df2, on='date')
+cluster_id = None 
+base_url = "http://localhost/kafka-rest-proxy"
+
+# Running this on startup 
+@app.on_event('startup')
+async def startup_event():
+    global cluster_id
+    response = requests.get(f"{base_url}/v3/clusters")
+    serializedData = response.json()
+    # set the global cluster id
+    cluster_id = serializedData["data"][0]["cluster_id"]
+    # return the value to the agent
+    return cluster_id
 
 
 @app.post('/')
@@ -19,15 +32,39 @@ def main():
 
 @app.get('/getClusterId')
 async def get_cluster_id(): 
-    response = requests.get("http://localhost/kafka-rest-proxy/v3/clusters")
+    global cluster_id
+    response = requests.get(f"{base_url}/v3/clusters")
     serializedData = response.json()
-    print(serializedData)
+    # set the global cluster id
+    cluster_id = serializedData["data"][0]["cluster_id"]
+    # return the value to the agent
+    return cluster_id
+
+# This code below is commented because it's not mandatory to create a topic before the dispatch of the event. If the topic does not exist, the proxy will create it itself. 
+
+# @app.get('/createTopic')  # Change get to post as you are creating a resource.
+# async def create_topic(topicName: str= "operational-domain-data"):
+#     payload = {
+#         "topic_name": topicName,
+#     }
+
+#     requestHeader = {
+#         'Content-Type': 'application/json'
+#     }
+    
+#     print(f"{base_url}/clusters/{cluster_id}/topics")
+#     response = requests.post(f"{base_url}/clusters/{cluster_id}/topics", headers=requestHeader, json=payload)
+
+#     if response.status_code != 200: 
+#         return {"error": response.text}
+
+#     return response.status_code # It's usually better to return the response rather than print it
 
 
 @app.post('/produce')
 async def produce_to_kafka(topic: str = 'test'):
     # Kafka REST Proxy URL for producing messages to a topic
-    url = "http://localhost/kafka-rest-proxy/topics/" + topic
+    url = f"${base_url}/kafka-rest-proxy/topics/" + topic
     headers = {
         'Content-Type': 'application/vnd.kafka.json.v2+json',
     }
