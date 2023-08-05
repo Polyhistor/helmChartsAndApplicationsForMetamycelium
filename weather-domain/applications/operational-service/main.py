b52ae11c70d37e0eea539a572877977ae0a597d0
Pouya Ataei <pouya-ataei@idexx.com>
2023-08-05 19:12:35 +1200

    fixed the issue with path for minIO library

--- weather-domain/applications/operational-service/main.py
+++ weather-domain/applications/operational-service/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 import pandas as pd
 import requests
-import json
+from minio import Minio
+from io import BytesIO
 
 app = FastAPI()
 
@@ -13,6 +14,14 @@ df2 = pd.read_csv('precipitation.csv')
 merged_df = pd.merge(df1, df2, on='date')
 cluster_id = None 
 base_url = "http://localhost/kafka-rest-proxy"
+
+# Initialize the Minio client
+minio_client = Minio(
+    "localhost:9001",
+    access_key="minioadmin",
+    secret_key="minioadmin",
+    secure=False
+)
 
 # Running this on startup 
 @app.on_event('startup')
@@ -30,6 +39,7 @@ async def startup_event():
 def main():
     return "Operational App"
 
+
 @app.get('/getClusterId')
 async def get_cluster_id(): 
     global cluster_id
@@ -44,24 +54,57 @@ async def get_cluster_id():
 @app.get('/store-operational-data')
 async def produce_to_kafka(topic: str = 'domain-weather-operational-data'):
     # Kafka REST Proxy URL for producing messages to a topic
-    url = f"${base_url}/kafka-rest-proxy/topics/" + topic
+    url = f"{base_url}/clusters/{cluster_id}/topics/" + topic
     headers = {
         'Content-Type': 'application/vnd.kafka.json.v2+json',
     }
 
+    # Store the merged data in Minio
+    csv_data = merged_df.to_csv(index=False).encode('utf-8')
+    csv_bytes = BytesIO(csv_data)
+
+    bucketExists = minio_client.bucket_exists("weather-domain-operational-data")
+    if not bucketExists:
+        minio_client.make_bucket("weather-domain-operational-data")
+    else: 
+        print("bucket already exist")
 
 
-@app.get('/test')
-def produce_test():
-    url = "http://localhost/kafka-rest-proxy/topics/test"
-    headers = {
-        'Content-Type': 'application/vnd.kafka.json.v2+json',
-    }
-    data = {
+    minio_client.put_object(
+        bucket_name='weather-domain-operational-data',
+        object_name='merged_data.csv',
+        data=csv_bytes,
+        length=len(csv_data),
+        content_type='text/csv',
+    )
+
+    # Dispatch the events
+    payload_start = {
         "records": [
-            {"value": 'I am a test event'}
+            {
+                "value": {
+                    "message": "Data loading started"
+                }
+            }
         ]
     }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    response = requests.post(url, headers=headers, data=json.dumps(payload_start))
     if response.status_code != 200:
-        raise Exception(f"POST /topics/test did not succeed: {response.text}")
+        return {"error": response.text}
+
+    payload_end = {
+        "records": [
+            {
+                "value": {
+                    "message": "Data loading finished"
+                }
+            }
+        ]
+    }
+    
+    response = requests.post(url, headers=headers, data=json.dumps(payload_end))
+    if response.status_code != 200:
+        return {"error": response.text}
+
+    return {"status": "Data loaded and events dispatched"}
