39fd8ffea1d50d2249f6665a386c9149ef1c93f5
Pouya Ataei <pouya-ataei@idexx.com>
2023-08-12 13:26:05 +1200

    working on the customer domain

--- customer-domain/applications/operational/main.py
+++ customer-domain/applications/operational/main.py
@@ -1,22 +1,14 @@
 from fastapi import FastAPI
-import pandas as pd
 import requests
 from minio import Minio
 from io import BytesIO
 import json
-import base64
-import time 
+import os
+import time
 
 app = FastAPI()
 
-
-
-# Load your CSV data
-df1 = pd.read_csv('temperature.csv')
-df2 = pd.read_csv('precipitation.csv')
-
-# Merge the dataframes on the 'date' column
-merged_df = pd.merge(df1, df2, on='date')
+# Constants
 cluster_id = None 
 kafka_rest_proxy_base_url = "http://localhost/kafka-rest-proxy"
 minio_url = "localhost:9001"
@@ -31,40 +23,36 @@ minio_client = Minio(
     secure=False
 )
 
-# Running this on startup 
 @app.on_event('startup')
 async def startup_event():
     global cluster_id
     response = requests.get(f"{kafka_rest_proxy_base_url}/v3/clusters")
     serializedData = response.json()
-    # set the global cluster id
     cluster_id = serializedData["data"][0]["cluster_id"]
-    # return the value to the agent
     return cluster_id
-
 
 @app.post('/')
 def main():
     return "Operational App"
-
 
 @app.get('/get-cluster-id')
 async def get_cluster_id(): 
     global cluster_id
     response = requests.get(f"{kafka_rest_proxy_base_url}/v3/clusters")
     serializedData = response.json()
-    # set the global cluster id
     cluster_id = serializedData["data"][0]["cluster_id"]
-    # return the value to the agent
     return cluster_id
 
 @app.get("/")
 async def main_function(): 
     return "welcome to the customer domain operational service"
 
+def list_json_files(directory='.'):
+    """Return a list of all JSON files in the directory."""
+    return [f for f in os.listdir(directory) if f.endswith('.json')]
+
 @app.get('/store-operational-data')
 async def produce_to_kafka(topic: str = 'domain-customer-operational-data'):
-    # Kafka REST Proxy URL for producing messages to a topic
     url = f"{kafka_rest_proxy_base_url}/topics/" + topic
     headers = {
         'Content-Type': 'application/vnd.kafka.json.v2+json',
@@ -74,7 +62,7 @@ async def produce_to_kafka(topic: str = 'domain-customer-operational-data'):
     payload_start = {
         "records": [
             {
-                "key" : "customer-domain-operational-data-stored",
+                "key": "customer-domain-operational-data-stored",
                 "value": {
                     "message": "Data loading started"
                 }
@@ -82,31 +70,38 @@ async def produce_to_kafka(topic: str = 'domain-customer-operational-data'):
         ]
     }
 
-    print(url)
     response = requests.post(url, headers=headers, data=json.dumps(payload_start))
-
     if response.status_code != 200:
         return {"error": response.text}
 
-    # Store the merged data in Minio
-    csv_data = merged_df.to_csv(index=False).encode('utf-8')
-    csv_bytes = BytesIO(csv_data)
+    # List all JSON files
+    json_files = list_json_files()
     min_io_bucket_name = "customer-domain-operational-data"
-    min_io_object_name = "merged_data-v2.5.csv"
 
+    # Check if bucket exists, if not, create one
     bucketExists = minio_client.bucket_exists(min_io_bucket_name)
     if not bucketExists:
         minio_client.make_bucket(min_io_bucket_name)
     else: 
-        print("bucket already exist")
+        print("bucket already exists")
 
-    minio_client.put_object(
-        bucket_name=min_io_bucket_name,
-        object_name=min_io_object_name,
-        data=csv_bytes,
-        length=len(csv_data),
-        content_type='text/csv',
-    )
+    # Iterate through each JSON file and store it in Minio
+    for json_file in json_files:
+        with open(json_file, 'r') as f:
+            data = json.load(f)
+        
+        # Convert the data to JSON string and encode it
+        json_data = json.dumps(data).encode('utf-8')
+        json_bytes = BytesIO(json_data)
+        min_io_object_name = json_file  # Using the filename as object name in Minio
+
+        minio_client.put_object(
+            bucket_name=min_io_bucket_name,
+            object_name=min_io_object_name,
+            data=json_bytes,
+            length=len(json_data),
+            content_type='application/json',
+        )
 
     # Dispatch the "Data loading finished" event
     payload_end = {
@@ -118,8 +113,7 @@ async def produce_to_kafka(topic: str = 'domain-customer-operational-data'):
                      "distributedStorageAddress" : minio_url,
                      "minio_access_key": minio_acces_key,
                      "minio_secret_key" : minio_secret_key,
-                     "bucket_name": min_io_bucket_name,
-                     "object_name" : min_io_object_name
+                     "bucket_name": min_io_bucket_name
                 }
             }
         ]
