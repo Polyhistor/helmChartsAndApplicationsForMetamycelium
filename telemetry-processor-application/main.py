797bf54ebf9b2c2f6d73ea0d780ace152f2a759a
Pouya Ataei <pouya-ataei@idexx.com>
2023-08-27 18:23:21 +1200

    updated the metrics from end to end to capture more from prometheus

--- telemetry-processor-application/main.py
+++ telemetry-processor-application/main.py
@@ -2,10 +2,12 @@ from http.client import HTTPException
 from xmlrpc.client import ResponseError
 from fastapi import FastAPI, BackgroundTasks, Response
 from minio import Minio
+import time
 import requests
 import json
 import base64
-from prometheus_client import Counter, start_http_server, generate_latest, CONTENT_TYPE_LATEST, Histogram
+from prometheus_client import Counter, start_http_server, generate_latest, CONTENT_TYPE_LATEST, Histogram, Gauge
+import psutil
 
 
 # Global variables
@@ -16,6 +18,9 @@ SERVICE_ADDRESS = "http://localhost:8008"
 kafka_records_consumed = Counter('kafka_records_consumed_total', 'Total Kafka records consumed')
 kafka_data_ingested_records = Counter('kafka_data_ingested_records_total', 'Number of Kafka records ingested')
 kafka_data_ingested_bytes = Counter('kafka_data_ingested_bytes_total', 'Number of bytes ingested from Kafka records')
+kafka_ingestion_errors = Counter('kafka_ingestion_errors_total', 'Number of errors while ingesting data')
+cpu_utilization_gauge = Gauge('service_cpu_utilization_percentage', 'CPU Utilization of the Service')
+memory_utilization_gauge = Gauge('service_memory_utilization_bytes', 'Memory (RAM) Utilization of the Service')
 
 KAFKA_PROCESSING_TIME = Histogram('kafka_processing_duration_seconds', 'Time taken for processing kafka messages')
 ingestion_latency = Histogram('kafka_ingestion_latency_seconds', 'Time taken from data creation to ingestion in seconds')
@@ -90,24 +95,37 @@ async def consume_kafka_message(background_tasks: BackgroundTasks):
 
     @KAFKA_PROCESSING_TIME.time()
     def consume_records():
-        
-        response = requests.get(url, headers=headers)
-        if response.status_code != 200:
-            raise Exception(f"GET /records/ did not succeed: {response.text}")
-        else:
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
             records = response.json()
+
             for record in records:
+                publish_time = record.get("timestamp", time.time())  # defaulting to current time if no timestamp
+                current_time = time.time()
+
+                # Calculating ingestion latency
+                latency = current_time - publish_time
+                ingestion_latency.observe(latency)
+
                 decoded_key = base64.b64decode(record['key']).decode('utf-8') if record['key'] else None
                 decoded_value_json = base64.b64decode(record['value']).decode('utf-8')
                 value_obj = json.loads(decoded_value_json)
-                print(decoded_key)
-                print(value_obj)
-                print(f"Consumed record with key {decoded_key} and value ")
 
-                # Increment the counter for each record consumed
+                # Data ingestion metrics
                 kafka_records_consumed.inc()
                 kafka_data_ingested_records.inc()
                 kafka_data_ingested_bytes.inc(len(json.dumps(record)))
+
+                print(f"Consumed record with key {decoded_key} and value {value_obj}")
+
+        except Exception as e:
+            kafka_ingestion_errors.inc()
+            raise Exception(f"Error while consuming data: {str(e)}")
+
+        # CPU & Memory Utilization
+        cpu_utilization_gauge.set(psutil.cpu_percent())
+        memory_utilization_gauge.set(psutil.virtual_memory().used)
 
     background_tasks.add_task(consume_records)
     return {"status": "Consuming records in the background"}
